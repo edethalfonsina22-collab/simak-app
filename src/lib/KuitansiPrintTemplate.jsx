8a4b5f551966e9e9fb4f5f22986e0965a539aa23
edethalfonsina22-collab <edeth.alfonsina22@admin.sd.belajar.id>
2026-08-20 22:53:32 +0900

    Update KuitansiPrintTemplate.jsx

--- src/lib/KuitansiPrintTemplate.jsx
+++ src/lib/KuitansiPrintTemplate.jsx
@@ -93,8 +93,8 @@ const KuitansiPrintTemplate = forwardRef(function KuitansiPrintTemplate({ sekola
       ref={ref}
       className="print-only relative bg-white text-black py-10 px-4 text-base overflow-hidden"
       style={{
-        width: '260mm',
-        minHeight: '148mm',
+        width: '280mm',
+        minHeight: '158mm',
         WebkitPrintColorAdjust: 'exact',
         printColorAdjust: 'exact',
         colorAdjust: 'exact',
